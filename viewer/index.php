2334104ce61eeb8961fba8f695e804c410c90552
Jaewon Kim <epiloum@gmail.com>
2023-05-25 23:23:40 +0900

    fix monthly cell bugs

--- viewer/index.php
+++ viewer/index.php
@@ -5,6 +5,26 @@ require_once('db.php');
 // 상수
 define('DAY_WIDTH', 20);
 
+// 함수
+function getMonthAbbreviation($monthNumber) {
+    $monthNames = [
+        1 => 'Jan',
+        2 => 'Feb',
+        3 => 'Mar',
+        4 => 'Apr',
+        5 => 'May',
+        6 => 'Jun',
+        7 => 'Jul',
+        8 => 'Aug',
+        9 => 'Sep',
+        10 => 'Oct',
+        11 => 'Nov',
+        12 => 'Dec'
+    ];
+
+    return $monthNames[$monthNumber] ?? 'Invalid month';
+}
+
 // 변수 처리
 $st = preg_match('/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/', $_GET['st'] ?? '')? strtotime($_GET['st']): strtotime(date('Y-m-d 00:00:00', time() - 86400*7));
 $ed = preg_match('/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/', $_GET['ed'] ?? '')? strtotime($_GET['ed']): strtotime(date('Y-m-d 00:00:00', $st + 86400*14)) + 86399;
@@ -23,18 +43,19 @@ for ($tt = $st; $tt <= $ed; $tt += 86400) {
     $wd = date('w', $tt);
     $month = date('n', $tt);
     $cnt_day++;
-    $cnt_month_span++;
 
     $header_day[] = '<td class="d' . $wd . '">' . $day . '</td>';
 
     if ($month != $curr_month) {
-        $header_month[] = '<td colspan="' . $cnt_month_span . '">' . $curr_month . '월</td>';
+        $header_month[] = '<td colspan="' . $cnt_month_span . '">' . getMonthAbbreviation($curr_month) . '</td>';
         $curr_month = $month;
         $cnt_month_span = 0;
     }
+    
+    $cnt_month_span++;
 }
 
-$header_month[] = '<td colspan="' . $cnt_month_span . '">' . $curr_month . '월</td>';
+$header_month[] = '<td colspan="' . $cnt_month_span . '">' . getMonthAbbreviation($curr_month) . '</td>';
 
 // 이슈 가져오기
 $issues = [];
@@ -72,7 +93,7 @@ while($v = $res->fetch_object()) {
 <title>JIRA Job Collector</title>
 <meta charset="UTF-8">
 <link rel="stylesheet" href="index.css" type="text/css" />
-<link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css" //>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css" />
 <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.min.js" integrity="sha256-eTyxS0rkjpLEo16uXTS0uVCS4815lc40K2iVpWDvdSY=" crossorigin="anonymous"></script>
 <script src="index.js" type="text/javascript"></script>
